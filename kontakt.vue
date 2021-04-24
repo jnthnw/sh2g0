369bbdddeb28b28587b2266bd2a1566962a398bf
jnthnw <wendler@campus.tu-berlin.de>
2021-04-24 16:39:15 +0200

    fixed contaxt

--- kontakt.vue
+++ kontakt.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <h1>KONTAKT</h1>
       <div class="row justify-content-md-center">
-        <div class="col-6 contact-box ">
+        <div class="col-12 col-lg-6 contact-box">
           <a
             href="https://www.facebook.com/Shakespeare-to-go-1812940565700467/"
           >
@@ -11,16 +11,16 @@
           >
 
           <div class="row">
-            <div class="col-6">ANSPRECHPARTNER</div>
-            <div class="col-6">Sonja Holzinger</div>
+            <div class="col-12 col-sm-6">ANSPRECHPARTNER</div>
+            <div class="col-12 col-sm-6">Sonja Holzinger</div>
           </div>
           <div class="row">
-            <div class="col-6">TEL</div>
-            <div class="col-6">+49 178 329 81 51</div>
+            <div class="col-12 col-sm-6">TEL</div>
+            <div class="col-12 col-sm-6">+49 178 329 81 51</div>
           </div>
           <div class="row">
-            <div class="col-6">MAIL</div>
-            <div class="col-6">sh2go.online@gmail.com</div>
+            <div class="col-12 col-sm-6">MAIL</div>
+            <div class="col-12 col-sm-6">sh2go.online@gmail.com</div>
           </div>
         </div>
       </div>
@@ -44,12 +44,22 @@ h2 {
   padding: 40px 40px;
 }
 
-.contact-box .row .col-6 {
-  text-align: right;
+@media (min-width: 575px) {
+  .contact-box .row div {
+    text-align: right;
+  }
+
+  .contact-box .row div:last-child {
+    text-align: left;
+  }
 }
 
-.contact-box .row .col-6:last-child {
-  text-align: left;
+.contact-box .row {
+  text-align: center !important;
+}
+
+.contact-box .row div:last-child {
+  padding-bottom: 20px;
 }
 
 .contact-box a {
