20c54f8020e5196675b309ea3c72c2c63ea8d71e
jnthnw <wendler@campus.tu-berlin.de>
2021-04-24 14:03:30 +0200

    moved icons added small navbar

--- header.vue
+++ header.vue
@@ -1,171 +1,195 @@
 <template>
-  <div class="_navbar navbar-expand-lg fixed-top headerbar">
+  <div class="navbar navbar-expand-xl fixed-top headerbar">
     <div class="container-fluid">
       <div class="text-center"></div>
+      <router-link class="nav-link" to="/">
+        <a class="navbar-brand" href="#"
+          ><img class="logo" src="assets/logo.webp" alt=""
+        /></a>
+      </router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="material-icons" style="color: white"> menu </span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="nav navbar-nav justify-content-center text-uppercase">
+          <router-link class="nav-link" to="/"> </router-link>
+          <li class="nav-item dropdown">
+            <a
+              class="_dropdown-toggle nav-link"
+              type="button"
+              href="#"
+              id="navbarScrollingDropdown"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Team
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+              <li>
+                <router-link class="nav-link" to="/team/sonja"
+                  >Sonja Holzinger</router-link
+                >
+                <router-link class="nav-link" to="/hannah"
+                  >David Gräber</router-link
+                >
+                <router-link class="nav-link" to="/hannah"
+                  >Andres Neu</router-link
+                >
+                <router-link class="nav-link" to="/team/hannah"
+                  >Hannah Müller</router-link
+                >
+                <router-link class="nav-link" to="/hannah"
+                  >Ehemalige</router-link
+                >
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="_dropdown-toggle nav-link"
+              type="button"
+              href="#"
+              id="filmeDropdown"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Filme
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="filmeDropdown">
+              <li>
+                <router-link class="nav-link" to="/film/morgen"
+                  >MORGEN IST AUCH NOCH EIN TAG</router-link
+                >
+                <router-link class="nav-link" to="/hannah"
+                  >CATTLEYA</router-link
+                >
+                <router-link class="nav-link" to="/hannah"
+                  >KEINE EXPERIMENTE</router-link
+                >
+                <router-link class="nav-link" to="/hannah"
+                  >ALLES WIRD GUT</router-link
+                >
+                <router-link class="nav-link" to="/hannah"
+                  >SUMMERNIGHT TALENTS</router-link
+                >
+                <router-link class="nav-link" to="/hannah"
+                  >GESTERN. HEUTE. MORGEN</router-link
+                >
+                <router-link class="nav-link" to="/hannah">ENGEL</router-link>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="_dropdown-toggle nav-link"
+              type="button"
+              href="#"
+              id="filmeDropdown"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Theater
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="filmeDropdown">
+              <li>
+                <router-link class="nav-link" to="/hannah">
+                  Schwestern
+                </router-link>
+                <router-link class="nav-link" to="/hannah">
+                  Preparadise
+                </router-link>
+                <router-link class="nav-link" to="/hannah">
+                  Wendezeit Zeitwende
+                </router-link>
+                <router-link class="nav-link" to="/hannah">
+                  Miranda
+                </router-link>
+                <router-link class="nav-link" to="/hannah">
+                  Macbeth
+                </router-link>
+                <router-link class="nav-link" to="/hannah">
+                  Insomnia
+                </router-link>
+                <router-link class="nav-link" to="/hannah">
+                  Miistrauen
+                </router-link>
+              </li>
+            </ul>
+          </li>
+          <!-- Show reel-->
+          <li class="nav-item dropdown">
+            <a
+              class="_dropdown-toggle nav-link"
+              type="button"
+              href="#"
+              id="filmeDropdown"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Showreel
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="filmeDropdown">
+              <li>
+                <router-link class="nav-link" to="/hannah">
+                  Eric Lust
+                </router-link>
+                <router-link class="nav-link" to="/hannah">
+                  The Missunderstanding
+                </router-link>
+                <router-link class="nav-link" to="/hannah">
+                  Popo Martin
+                </router-link>
+              </li>
+            </ul>
+          </li>
 
-      <ul class="nav navbar-nav justify-content-center text-uppercase">
-        <router-link class="nav-link" to="/">
-          <img
-            class="logo"
-            src="https://static.wixstatic.com/media/758d99_51aa420ab5384d4583d8ee0e6a59c5c7~mv2.jpg/v1/crop/x_642,y_189,w_610,h_671/fill/w_181,h_169,al_c,q_80,usm_0.66_1.00_0.01/LOGO19.webp"
-            alt=""
-          />
-        </router-link>
-        <li class="nav-item dropdown">
-          <a
-            class="_dropdown-toggle nav-link"
-            type="button"
-            href="#"
-            id="navbarScrollingDropdown"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            Team
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
-            <li>
-              <router-link class="nav-link" to="/team/sonja">Sonja Holzinger</router-link>
-              <router-link class="nav-link" to="/hannah">David Gräber</router-link>
-              <router-link class="nav-link" to="/hannah">Andres Neu</router-link>
-              <router-link class="nav-link" to="/team/hannah">Hannah Müller</router-link>
-              <router-link class="nav-link" to="/hannah">Ehemalige</router-link>
+          <li class="nav-item dropdown">
+            <a
+              class="_dropdown-toggle nav-link"
+              type="button"
+              href="#"
+              id="filmeDropdown"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Service
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="filmeDropdown">
+              <li>
+                <router-link class="nav-link" to="/hannah">
+                  Allgemein
+                </router-link>
 
-            </li>
-          </ul>
-        </li>
-        <li class="nav-item dropdown">
-          <a
-            class="_dropdown-toggle nav-link"
-            type="button"
-            href="#"
-            id="filmeDropdown"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            Filme
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="filmeDropdown">
-            <li>
-              <router-link class="nav-link" to="/film/morgen"
-                >MORGEN IST AUCH NOCH EIN TAG</router-link
-              >
-              <router-link class="nav-link" to="/hannah">CATTLEYA</router-link>
-              <router-link class="nav-link" to="/hannah"
-                >KEINE EXPERIMENTE</router-link
-              >
-              <router-link class="nav-link" to="/hannah"
-                >ALLES WIRD GUT</router-link
-              >
-              <router-link class="nav-link" to="/hannah"
-                >SUMMERNIGHT TALENTS</router-link
-              >
-              <router-link class="nav-link" to="/hannah"
-                >GESTERN. HEUTE. MORGEN</router-link
-              >
-              <router-link class="nav-link" to="/hannah">ENGEL</router-link>
-            </li>
-          </ul>
-        </li>
-        <li class="nav-item dropdown">
-          <a
-            class="_dropdown-toggle nav-link"
-            type="button"
-            href="#"
-            id="filmeDropdown"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            Theater
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="filmeDropdown">
-            <li>
-              <router-link class="nav-link" to="/hannah">
-                Schwestern
-              </router-link>
-              <router-link class="nav-link" to="/hannah">
-                Preparadise
-              </router-link>
-              <router-link class="nav-link" to="/hannah">
-                Wendezeit Zeitwende
-              </router-link>
-              <router-link class="nav-link" to="/hannah">
-                Miranda
-              </router-link>
-              <router-link class="nav-link" to="/hannah">
-                Macbeth
-              </router-link>
-              <router-link class="nav-link" to="/hannah">
-                Insomnia
-              </router-link>
-              <router-link class="nav-link" to="/hannah">
-                Miistrauen
-              </router-link>
-            </li>
-          </ul>
-        </li>
-        <!-- Show reel-->
-        <li class="nav-item dropdown">
-          <a
-            class="_dropdown-toggle nav-link"
-            type="button"
-            href="#"
-            id="filmeDropdown"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            Showreel
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="filmeDropdown">
-            <li>
-              <router-link class="nav-link" to="/hannah">
-                Eric Lust
-              </router-link>
-              <router-link class="nav-link" to="/hannah">
-                The Missunderstanding
-              </router-link>
-              <router-link class="nav-link" to="/hannah">
-                Popo Martin
-              </router-link>
-            </li>
-          </ul>
-        </li>
+                <router-link class="nav-link" to="/hannah">
+                  Drehbuch
+                </router-link>
+              </li>
+            </ul>
+          </li>
 
-        <li class="nav-item dropdown">
-          <a
-            class="_dropdown-toggle nav-link"
-            type="button"
-            href="#"
-            id="filmeDropdown"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            Service
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="filmeDropdown">
-            <li>
-              <router-link class="nav-link" to="/hannah">
-                Allgemein
-              </router-link>
-
-              <router-link class="nav-link" to="/hannah">
-                Drehbuch
-              </router-link>
-            </li>
-          </ul>
-        </li>
-
-        <li class="nav-item">
-          <router-link class="nav-link" to="/aktuelles">Aktuelles</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/kontakt">Kontakt</router-link>
-        </li>
-      </ul>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/aktuelles"
+              >Aktuelles</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/kontakt">Kontakt</router-link>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -178,11 +202,13 @@ export default {};
   background-color: black;
 }
 .nav {
+  background-color: black;
   margin-bottom: 20px;
   margin-top: 10px;
 }
 
 .nav-link {
+  background-color: black;
   color: white;
   font-family: montserrat-extralight;
   font-size: 30px;
@@ -206,7 +232,7 @@ export default {};
 }
 
 .dropdown-menu {
-  background-color: black;
+  background-color: black !important;
   width: 300px;
 }
 
