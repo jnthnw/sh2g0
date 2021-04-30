c8a02820392d7a0b1815b7ccd8645ea7d2a05dd2
jnthnw <wendler@campus.tu-berlin.de>
2021-04-30 11:38:06 +0200

    team base

--- team/hannah.vue
+++ team/hannah.vue
@@ -1,50 +1,22 @@
 <template>
-  <div class="main-content">
-    <div class="container">
-      <h1>Hannah Müller</h1>
-      <div class="row pictext">
-        <div class="col-12 col-md-6">
-          <img src="img/hannah.jpg" class="pic" alt="" />
-        </div>
-        <div class="col-12 col-md-6 text">
-          <span class="">
-            Hannah Müller ist in Stuttgart aufgewachsen und studiert Theater-
-            und Veranstaltungstechnik an der Beuth Hochschule für Technik
-            Berlin. Sie hat bereits bei einigen freien Filmproduktionen
-            mitge-arbeitet und ist beim Rundfunk Berlin Brandenburg als
-            Technische Hilfskraft angestellt. Außerdem ist sie Hobbykünstlerin.
-            Bei Shakespeare2go übernimmt sie die technische Leitung von Theater-
-            und Filmprojekten und unterstützt Sonja Holzinger in der Produktion.
-          </span>
-        </div>
-      </div>
-    </div>
-  </div>
+  <teambase full_name="Hanna Müller" image="hannah.jpg">
+    <template v-slot:content>
+      Hannah Müller ist in Stuttgart aufgewachsen und studiert Theater- und
+      Veranstaltungstechnik an der Beuth Hochschule für Technik Berlin. Sie hat
+      bereits bei einigen freien Filmproduktionen mitge-arbeitet und ist beim
+      Rundfunk Berlin Brandenburg als Technische Hilfskraft angestellt. Außerdem
+      ist sie Hobbykünstlerin. Bei Shakespeare2go übernimmt sie die technische
+      Leitung von Theater- und Filmprojekten und unterstützt Sonja Holzinger in
+      der Produktion.
+    </template>
+  </teambase>
 </template>
-<style scoped>
-h1 {
-  color: white;
-  position: relative;
-  font-size: 80px;
-  text-shadow: 0x 3px 10px gray;
-}
-
-.pic {
-  filter: grayscale();
-  width: 100%;
-}
-.pictext {
-  z-index: -1;
-  position: relative;
-  margin-top: -60px;
-}
-
-.text {
-  margin-top: 50px;
-  font-size: 20px;
-}
-
-.container {
-  color: white;
-}
-</style>
+<script>
+export default {
+  components: {
+    teambase: Vue.defineAsyncComponent(() =>
+      loadModule("./team/team_base.vue", options)
+    ),
+  },
+};
+</script>
---
+++ team/team_base.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="main-content">
+    <div class="container">
+      <h1>{{full_name}}</h1>
+      <div class="row pictext">
+        <div class="col-12 col-md-6">
+          <img :src="'img/'+image" class="pic" alt="" />
+        </div>
+        <div class="col-12 col-md-6 text">
+          <span class="">
+              <slot name="content"/>
+              
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    props: [
+        "full_name",
+        "image"
+    ]
+}
+</script>
+<style>
+h1 {
+  color: white;
+  position: relative;
+  font-size: 80px;
+  text-shadow: 0x 3px 10px gray;
+}
+
+.pic {
+  filter: grayscale();
+  width: 100%;
+}
+.pictext {
+  z-index: -1;
+  position: relative;
+  margin-top: -60px;
+}
+
+.text {
+  margin-top: 50px;
+  font-size: 20px;
+}
+
+.container {
+  color: white;
+}
+</style>
